a6614a7cdd67e3033798942724332372428691d8
supermanc88 <supermanc88@gmail.com>
2018-07-06 10:25:58 +0800

    添加dynamic动态库相关框架代码

--- FileDiskDynamic/FileDiskDynamic.cpp
+++ FileDiskDynamic/FileDiskDynamic.cpp
@@ -1,6 +1,44 @@
 // FileDiskDynamic.cpp : 定义 DLL 应用程序的导出函数。
 //
-
 #include "stdafx.h"
+#include "FileDiskDynamic.h"
+
+HANDLE g_hPort = INVALID_HANDLE_VALUE;
+
+extern "C" __declspec(dllexport) int InitialCommunicationPort(void)
+{
+	DWORD hResult = FilterConnectCommunicationPort(
+		NPMINI_PORT_NAME,
+		0,
+		NULL,
+		0,
+		NULL,
+		&g_hPort);
+
+	if (hResult != S_OK) {
+		return hResult;
+	}
+	return 0;
+}
+
+extern "C" __declspec(dllexport) int NPSendMessage(PVOID InputBuffer)
+{
+	DWORD bytesReturned = 0;
+	DWORD hResult = 0;
+	PCOMMAND_MESSAGE commandMessage = (PCOMMAND_MESSAGE)InputBuffer;
+
+	hResult = FilterSendMessage(
+		g_hPort,
+		commandMessage,
+		sizeof(COMMAND_MESSAGE),
+		NULL,
+		NULL,
+		&bytesReturned);
+
+	if (hResult != S_OK) {
+		return hResult;
+	}
+	return 0;
+}
 
 
---
+++ FileDiskDynamic/FileDiskDynamic.h
@@ -0,0 +1,43 @@
+#ifndef _FILEDISKDYNAMIC_H_
+#define _FILEDISKDYNAMIC_H_
+
+#include <windows.h>
+#include <stdio.h>
+#include <FltUser.h>
+
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "kernel32.lib")
+#pragma comment(lib, "fltLib.lib")
+#pragma comment(lib, "fltMgr.lib")
+#pragma comment(lib, "ntoskrnl.lib")
+#pragma comment(lib, "hal.lib")
+
+extern HANDLE g_hPort;
+
+#define NPMINI_NAME            L"NPminifilter"
+#define NPMINI_PORT_NAME       L"\\NPMiniPort"
+
+typedef enum _NPMINI_COMMAND {
+	ENUM_PASS = 0,
+	ENUM_BLOCK
+} NPMINI_COMMAND;
+
+typedef struct _COMMAND_MESSAGE {
+	NPMINI_COMMAND 	Command;
+} COMMAND_MESSAGE, *PCOMMAND_MESSAGE;
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__declspec(dllexport)	int InitialCommunicationPort(void);
+__declspec(dllexport)   int NPSendMessage(PVOID InputBuffer);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !_FILEDISKDYNAMIC_H_
+
