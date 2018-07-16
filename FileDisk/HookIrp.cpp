3696033010780e043e56dd976d743b69f4ddfa87
supermanc88 <supermanc88@gmail.com>
2018-07-16 16:07:12 +0800

    添加根据devicename获取符号链接的相关代码

--- FileDisk/HookIrp.cpp
+++ FileDisk/HookIrp.cpp
@@ -1,134 +1,148 @@
 #include "HookIrp.h"
 
 
-extern POBJECT_TYPE *IoDriverObjectType;
+/************************************************************************/
+/* 目前弃用，可能有多次提示插入的情况                                      */
+/************************************************************************/
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-	typedef NTSTATUS(*DISPATCH_ENTRY)(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp);
-
-	DISPATCH_ENTRY Hook_Old_Entry_DispatchPnp_Disk = NULL;
-	/************************************************************************/
-	/*        未公开函数                                                     */
-
-	NTKERNELAPI
-		NTSTATUS
-		NTAPI
-		ObReferenceObjectByName(
-		IN PUNICODE_STRING	ObjectName,
-		IN ULONG	 Attributes,
-		IN PACCESS_STATE	PassedAccessState OPTIONAL,
-		IN ACCESS_MASK	 DesiredAccess OPTIONAL,
-		IN POBJECT_TYPE	 ObjectType OPTIONAL,
-		IN KPROCESSOR_MODE	AccessMode,
-		IN OUT PVOID	 ParseContext OPTIONAL,
-		OUT	PVOID	 *Object
-		);
-	/************************************************************************/
-
-
-NTSTATUS GetDriverObject(IN PWCHAR DeviceName, OUT PDRIVER_OBJECT *DriverObject)
-{
-	NTSTATUS        Status;
-	UNICODE_STRING  uDeviceName;
-	RtlInitUnicodeString(&uDeviceName, DeviceName);
-
-	Status = ObReferenceObjectByName(
-		&uDeviceName,
-		OBJ_CASE_INSENSITIVE,
-		NULL,
-		0,
-		(POBJECT_TYPE)*IoDriverObjectType,
-		KernelMode,
-		NULL,
-		(PVOID *)DriverObject);
-
-	//ObDereferenceObject( *DriverObject );
-
-	return Status;
-}
-
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-BOOLEAN
-	HookIRP_Install(VOID)
-{
-	NTSTATUS        Status;
-	PDRIVER_OBJECT  DriverObject = NULL;
-	KIRQL           Irql;
-
-
-	// -- Hook Disk
-	Status = GetDriverObject(HOOK_DRIVER_NAME, &DriverObject);
-	if (!NT_SUCCESS(Status))
-	{
-		KdPrint(("HookIRP_Install: GetDeviceObjectEx (Driver\\Disk) error!!\n"));
-		return FALSE;
-	}
-
-	KeRaiseIrql(HIGH_LEVEL, &Irql);
-
-	//hook 即时插拔
-	Hook_Old_Entry_DispatchPnp_Disk = (DISPATCH_ENTRY)DriverObject->MajorFunction[IRP_MJ_PNP];
-
-	if (Hook_Old_Entry_DispatchPnp_Disk)
-	{
-		InterlockedExchangePointer((PVOID*)&DriverObject->MajorFunction[IRP_MJ_PNP], (PVOID)HookIrp_DispatchPnpDisk);
-	}
-
-	KeLowerIrql(Irql);
-
-	ObfDereferenceObject(DriverObject);
-	DriverObject = NULL;
-
-	KdPrint(("[+++++]HookIRP_install Disk: Read(0x%x) Write(0x%x) .\n", Hook_Old_Entry_DispatchRead_Disk, Hook_Old_Entry_DispatchWrite_Disk));
-
-	return TRUE;
-}
-
-
-NTSTATUS HookIrp_DispatchPnpDisk(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
-{
-	NTSTATUS                   Status = STATUS_UNSUCCESSFUL;
-	PIO_STACK_LOCATION         IrpSp = NULL;
-	DVCLOCK_DEVICE_INFO_EX    *DeviceInfo = NULL;
-	UCHAR IrpMini = 0xFF; //避免使用和 IRP_MN_XXx冲突的初始化值
-	ULONG sidSize = 0;
-	IrpSp = IoGetCurrentIrpStackLocation(Irp);
-
-	IrpMini = IrpSp->MinorFunction;
-
-	KdPrint(("HookIrp_DispatchPnpDisk enter....\n"));
-
-	Status = HookIrp_CallPnpDisk(DeviceObject, Irp);
-	if (NT_SUCCESS(Status))
-	{
-		KdPrint(("HookIrp_DispatchPnpDisk:%08x, PID:%08x\n", IrpMini, (ULONG)PsGetCurrentProcessId()));
-		switch (IrpMini)
-		{
-		case  IRP_MN_START_DEVICE:
-			// -- 从链表中查找磁盘设备信息
-			DeviceInfo = DistillDvcLockDeviceInfo(DeviceObject, Irp, IRP_DVCLOCK_DISK_READ_WRITE);
-			if (NULL != DeviceInfo)
-			{
-				DvcLockFree(DeviceInfo);
-				DeviceInfo = NULL;
-			}
-			break;
-		case IRP_MN_REMOVE_DEVICE:
-			KdPrint(("IRP_MN_REMOVE_DEVICE\n"));
-			RemoveDvcLockDeviceInfoList(DeviceObject);
-			break;
-		default:
-			break;
-		}
-	}
-	return Status;
-}
-
-#ifdef __cplusplus
-}
-#endif
+// extern POBJECT_TYPE *IoDriverObjectType;
+// 
+// #ifdef __cplusplus
+// extern "C"
+// {
+// #endif
+// 
+// 	typedef NTSTATUS(*DISPATCH_ENTRY)(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp);
+// 
+// 	DISPATCH_ENTRY Hook_Old_Entry_DispatchPnp_Disk = NULL;
+// 	/************************************************************************/
+// 	/*        未公开函数                                                     */
+// 
+// 	NTKERNELAPI
+// 		NTSTATUS
+// 		NTAPI
+// 		ObReferenceObjectByName(
+// 		IN PUNICODE_STRING	ObjectName,
+// 		IN ULONG	 Attributes,
+// 		IN PACCESS_STATE	PassedAccessState OPTIONAL,
+// 		IN ACCESS_MASK	 DesiredAccess OPTIONAL,
+// 		IN POBJECT_TYPE	 ObjectType OPTIONAL,
+// 		IN KPROCESSOR_MODE	AccessMode,
+// 		IN OUT PVOID	 ParseContext OPTIONAL,
+// 		OUT	PVOID	 *Object
+// 		);
+// 	/************************************************************************/
+// 
+// 
+// NTSTATUS GetDriverObject(IN PWCHAR DeviceName, OUT PDRIVER_OBJECT *DriverObject)
+// {
+// 	NTSTATUS        Status;
+// 	UNICODE_STRING  uDeviceName;
+// 	RtlInitUnicodeString(&uDeviceName, DeviceName);
+// 
+// 	Status = ObReferenceObjectByName(
+// 		&uDeviceName,
+// 		OBJ_CASE_INSENSITIVE,
+// 		NULL,
+// 		0,
+// 		(POBJECT_TYPE)*IoDriverObjectType,
+// 		KernelMode,
+// 		NULL,
+// 		(PVOID *)DriverObject);
+// 
+// 	//ObDereferenceObject( *DriverObject );
+// 
+// 	return Status;
+// }
+// 
+// //////////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////////
+// BOOLEAN
+// 	HookIRP_Install(VOID)
+// {
+// 	NTSTATUS        Status;
+// 	PDRIVER_OBJECT  DriverObject = NULL;
+// 	KIRQL           Irql;
+// 
+// 
+// 	// -- Hook Disk
+// 	Status = GetDriverObject(HOOK_DRIVER_NAME, &DriverObject);
+// 	if (!NT_SUCCESS(Status))
+// 	{
+// 		KdPrint(("HookIRP_Install: GetDeviceObjectEx (Driver\\Disk) error!!\n"));
+// 		return FALSE;
+// 	}
+// 
+// 	KeRaiseIrql(HIGH_LEVEL, &Irql);
+// 
+// 	//hook 即时插拔
+// 	Hook_Old_Entry_DispatchPnp_Disk = (DISPATCH_ENTRY)DriverObject->MajorFunction[IRP_MJ_PNP];
+// 
+// 	if (Hook_Old_Entry_DispatchPnp_Disk)
+// 	{
+// 		InterlockedExchangePointer((PVOID*)&DriverObject->MajorFunction[IRP_MJ_PNP], (PVOID)HookIrp_DispatchPnpDisk);
+// 	}
+// 
+// 	KeLowerIrql(Irql);
+// 
+// 	ObfDereferenceObject(DriverObject);
+// 	DriverObject = NULL;
+// 
+// 
+// 	return TRUE;
+// }
+// 
+// 
+// NTSTATUS HookIrp_DispatchPnpDisk(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
+// {
+// 	NTSTATUS                   Status = STATUS_UNSUCCESSFUL;
+// 	PIO_STACK_LOCATION         IrpSp = NULL;
+// 	DVCLOCK_DEVICE_INFO_EX    *DeviceInfo = NULL;
+// 	UCHAR IrpMini = 0xFF; //避免使用和 IRP_MN_XXx冲突的初始化值
+// 	ULONG sidSize = 0;
+// 	IrpSp = IoGetCurrentIrpStackLocation(Irp);
+// 
+// 	IrpMini = IrpSp->MinorFunction;
+// 
+// 	KdPrint(("HookIrp_DispatchPnpDisk enter....\n"));
+// 
+// 	Status = HookIrp_CallPnpDisk(DeviceObject, Irp);
+// 	if (NT_SUCCESS(Status))
+// 	{
+// 		KdPrint(("HookIrp_DispatchPnpDisk:%08x, PID:%08x\n", IrpMini, (ULONG)PsGetCurrentProcessId()));
+// 		switch (IrpMini)
+// 		{
+// 		case  IRP_MN_START_DEVICE:
+// 			// -- 从链表中查找磁盘设备信息
+// 			DeviceInfo = DistillDvcLockDeviceInfo(DeviceObject, Irp, IRP_DVCLOCK_DISK_READ_WRITE);
+// 			if (NULL != DeviceInfo)
+// 			{
+// 				DvcLockFree(DeviceInfo);
+// 				DeviceInfo = NULL;
+// 			}
+// 			break;
+// 		case IRP_MN_REMOVE_DEVICE:
+// 			KdPrint(("IRP_MN_REMOVE_DEVICE\n"));
+// 			RemoveDvcLockDeviceInfoList(DeviceObject);
+// 			break;
+// 		default:
+// 			break;
+// 		}
+// 	}
+// 	return Status;
+// }
+// 
+// 
+// 
+// NTSTATUS
+// HookIrp_CallPnpDisk(
+// IN PDEVICE_OBJECT DeviceObject,
+// IN PIRP Irp
+// )
+// {
+// 	return ((DISPATCH_ENTRY)Hook_Old_Entry_DispatchPnp_Disk)(DeviceObject, Irp);
+// }
+// 
+// #ifdef __cplusplus
+// }
+// #endif
--- FileDisk/HookIrp.h
+++ FileDisk/HookIrp.h
@@ -1,34 +1,84 @@
-#ifndef _HOOKIRP_H_
-#define _HOOKIRP_H_
-
-#include <ntddk.h>
-
-#define HOOK_DRIVER_NAME     L"\\Driver\\Disk" 
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-//
-// -- HOOK DISK.SYS 回调例程
-//
-BOOLEAN
-	HookIRP_Install(VOID);
-
-//
-// -- HOOK DISK.SYS 回调例程
-//
-BOOLEAN
-	HookIRP_Uninstall(VOID);
-
-NTSTATUS
-HookIrp_DispatchPnpDisk(
-IN PDEVICE_OBJECT DeviceObject,
-IN PIRP Irp
-);
-#ifdef __cplusplus
-}
-#endif
-
-#endif // !_HOOKIRP_H_
+// #ifndef _HOOKIRP_H_
+// #define _HOOKIRP_H_
+// 
+// #include <ntddk.h>
+// 
+// #define HOOK_DRIVER_NAME     L"\\Driver\\Disk" 
+// 
+// #ifdef __cplusplus
+// extern "C"
+// {
+// #endif
+// 
+// 
+// typedef struct _DVCLOCK_DEVICE_INFO_EX{
+// 
+// 	// -- 链表
+// 	struct _DVCLOCK_DEVICE_INFO_EX *Next;
+// 
+// 	// -- 设备对象信息
+// 	PVOID					        DeviceObject;           //设备对象(PDEVICE_OBJECT)
+// 	PVOID                           DeviceVpb;              //
+// 	BYTE                            DeviceBusType;          //设备总线类型
+// 
+// 	BOOLEAN                         PasswordCheckd;         //密码已经验证
+// 	BOOLEAN                         WirteProtect;           //是否写保护
+// 
+// 	BOOLEAN                         EnableUse;              //是否被禁用
+// 	ULONG                           RegType;                //注册类型 EMS_REG_TYPE_XXXX
+// 	ULONG                           UseAuthority;           //使用权限 EMS_AUTHORITY_XXX
+// 	BYTE                            DiskSN[DISK_SN_LENGTH]; //磁盘序列号 ------------------------------为么不为CHAR类型
+// 
+// 	BYTE                            ExceptionUDisk;         //例外设备
+// 
+// 	// -- 磁盘设备对象
+// 	BYTE                            DiskConIndex[CONFILE_INDEX_LEN]; //CConIndex
+// 	BYTE                            TempConIndex[CONFILE_INDEX_LEN]; //替换1AD区域数据
+// 	PVOID                           DiskConFile;            //(CConFile *) 
+// 	PVOID                           Disk0Sector;            //磁盘0扇区内容
+// 	PVOID                           Disk0SectorSrc;         //磁盘0扇区原始内容/*0扇区解密*/
+// 
+// 	BYTE                            ResetDiskCon;           //是否重新读取
+// 	BYTE                            EncDiskFlags;			//Modify shizhiq 加密U盘标记 审计中 删除中 可用中
+// 	BYTE							NewEncFlags;
+// 	// -- 保留
+// 	PVOID                           Expand;                 //扩展
+// 	DWORD                           Reserved;               //保留
+// 	LARGE_INTEGER  ReadTime;
+// 	LARGE_INTEGER  WriteTime;
+// 	LARGE_INTEGER FirstWirteTime; //记录磁盘读写差值 500毫秒
+// 	struct _DVCLOCK_DEVICE_INFO_EX * pListNode;
+// 	BOOLEAN blTimeOut; //是否超时从挂卷的时候执行写操作控制，3分钟写操作时间。超过就执行对应的策略
+// 	BOOLEAN blMount; //是否mount上
+// }DVCLOCK_DEVICE_INFO_EX, *PDVCLOCK_DEVICE_INFO_EX;
+// 
+// //
+// // -- HOOK DISK.SYS 回调例程
+// //
+// BOOLEAN
+// 	HookIRP_Install(VOID);
+// 
+// //
+// // -- HOOK DISK.SYS 回调例程
+// //
+// BOOLEAN
+// 	HookIRP_Uninstall(VOID);
+// 
+// NTSTATUS
+// HookIrp_DispatchPnpDisk(
+// IN PDEVICE_OBJECT DeviceObject,
+// IN PIRP Irp
+// );
+// 
+// 
+// NTSTATUS
+// HookIrp_CallPnpDisk(
+// IN PDEVICE_OBJECT DeviceObject,
+// IN PIRP Irp
+// );
+// 
+// #ifdef __cplusplus
+// }
+// #endif
+// 
+// #endif // !_HOOKIRP_H_
